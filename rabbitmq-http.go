6b292c2bf4925f508490001f51e3832e2b3982b5
Poorna Chandra Tejasvi KG <pct960@gmail.com>
2017-12-15 12:02:29 +0530

    Update rabbitmq-http.go

--- rabbitmq-http.go
+++ rabbitmq-http.go
@@ -351,30 +351,30 @@ func PublishHandler(w http.ResponseWriter, r *http.Request) {
 		//		return
 		//}
 
-		select
-		{
-		case ch:=<-cFail:
-			log.Printf(r.Header.Get("X-Real-Ip")+" "+r.Header.Get("X-Consumer-Id")+" "+r.Header.Get("X-Consumer-Username")+" "+r.Header.Get("Apikey")+" Incorrect exchange or queue name")
-			http.Error(w,"Incorrect exchange or queue name "+ch.ReplyText,http.StatusBadRequest)
-			return
-		case <-time.After(100*time.Millisecond):
-			w.Write([]byte("Publish message OKdkjvfbfwjln\n"))
-			return
-		}
-
-
 		//select
 		//{
 		//case ch:=<-cFail:
 		//	log.Printf(r.Header.Get("X-Real-Ip")+" "+r.Header.Get("X-Consumer-Id")+" "+r.Header.Get("X-Consumer-Username")+" "+r.Header.Get("Apikey")+" Incorrect exchange or queue name")
 		//	http.Error(w,"Incorrect exchange or queue name "+ch.ReplyText,http.StatusBadRequest)
 		//	return
-		//case ch1:=<-cPass:
-		//	if ch1.Ack {
-		//		w.Write([]byte("Publish message OK "))
-		//		return
-		//	}
+		//case <-time.After(100*time.Millisecond):
+		//	w.Write([]byte("Publish message OKdkjvfbfwjln\n"))
+		//	return
 		//}
+
+
+		select
+		{
+		case ch:=<-cFail:
+			log.Printf(r.Header.Get("X-Real-Ip")+" "+r.Header.Get("X-Consumer-Id")+" "+r.Header.Get("X-Consumer-Username")+" "+r.Header.Get("Apikey")+" Incorrect exchange or queue name")
+			http.Error(w,"Incorrect exchange or queue name "+ch.ReplyText,http.StatusBadRequest)
+			return
+		case ch1:=<-cPass:
+			if ch1.Ack {
+				w.Write([]byte("Publish message OK "))
+				return
+			}
+		}
 
 	} else {
 		w.WriteHeader(http.StatusMethodNotAllowed)
