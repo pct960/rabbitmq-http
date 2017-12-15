864407707dee2c9ea4d0fd31649729b957b1f832
Poorna Chandra Tejasvi KG <pct960@gmail.com>
2017-12-15 13:57:50 +0530

    Update rabbitmq-http.go

--- rabbitmq-http.go
+++ rabbitmq-http.go
@@ -338,15 +338,18 @@ func PublishHandler(w http.ResponseWriter, r *http.Request) {
 
 		rabbit.channel.NotifyReturn(cFail)
 
-		select
+		for
 		{
-		case ch:=<-cFail:
-			log.Printf(r.Header.Get("X-Real-Ip")+" "+r.Header.Get("X-Consumer-Id")+" "+r.Header.Get("X-Consumer-Username")+" "+r.Header.Get("Apikey")+" Incorrect exchange or queue name")
-			http.Error(w,"Incorrect exchange or queue name "+ch.ReplyText,http.StatusBadRequest)
-			return
-		case <-time.After(2*time.Second):
-			w.Write([]byte("Publish message OK\n"))
-			return
+			select
+			{
+			case ch:=<-cFail:
+				log.Printf(r.Header.Get("X-Real-Ip")+" "+r.Header.Get("X-Consumer-Id")+" "+r.Header.Get("X-Consumer-Username")+" "+r.Header.Get("Apikey")+" Incorrect exchange or queue name")
+				http.Error(w,"Incorrect exchange or queue name "+ch.ReplyText,http.StatusBadRequest)
+				return
+			case <-time.After(100*time.Millisecond):
+				w.Write([]byte("Publish message OK\n"))
+				return
+			}
 		}
 
 
